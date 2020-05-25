c5179dfe16d85d65b8d0f63aace7a0d0f6563f3c
James <jjahammond@hotmail.co.uk>
2020-05-25 23:36:33 +0100

    Connecting to Atlas MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
-const express = require('express'),
-      app = express();
+const express     = require('express'),
+      mongoose    = require('mongoose'),
+      dotenv      = require('dotenv'),
+      bodyParser  = require('body-parser'),
+      Email       = require('./models/email');
+
+const app = express();
+dotenv.config();
 
 // Middleware mounting
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// mongodb+srv://jjahammond:<password>@studiodoor-0rdn1.mongodb.net/test?retryWrites=true&w=majority
+const mongoDBURL = process.env.MONGODB_URL;
+mongoose.connect(mongoDBURL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to database');
+}).catch(err => {
+  console.log('ERROR:', err.message);
+});
+
 
 app.get("/", (req, res) => {
   res.render("index");
-})
+});
+
+app.post("/", (req, res) => {
+  newEmail = req.body;
+
+  // Save to database
+  Email.findOne(newEmail, (err, foundOne) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (!foundOne) {
+        Email.create(newEmail, (err, newCreated) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Email added to db");
+            res.redirect("/");
+          }
+        });
+      } else {
+        console.log("Email exists in db");
+        res.redirect("/");
+      }
+    }
+  });
+});
 
 // *************  Request listener *******************
 const PORT = process.env.PORT || 5000
---
+++ models/email.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+var emailSchema = new mongoose.Schema({
+  email: String,
+  });
+
+module.exports = mongoose.model("Email", emailSchema);
