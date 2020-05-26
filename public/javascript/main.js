2a411d6a034b7d6565dd5e5ab4a0714b704ee60d
James <jjahammond@hotmail.co.uk>
2020-05-26 21:56:48 +0100

    iOS autoplay fix v2

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
-  $("#image").attr("src", "assets/landing-page-animation-cropped.gif?" + Math.random());
-});
-
-function fallback(video)
-{
-  var img = video.querySelector('img');
-  if (img)
-    video.parentNode.replaceChild(img, video);
-}
+// $(document).ready(function () {
+//   $("#image").attr("src", "assets/landing-page-animation-cropped.gif?" + Math.random());
+// });
+//
+// function fallback(video)
+// {
+//   var img = video.querySelector('img');
+//   if (img)
+//     video.parentNode.replaceChild(img, video);
+// }
